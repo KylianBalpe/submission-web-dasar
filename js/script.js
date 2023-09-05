441a8dfe8e48c9d7683d1e79e4cf4cbdb6429236
Iqbal Pamula <balpamula@gmail.com>
2023-09-05 23:41:07 +0700

    final

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuButton = document.getElementById("menu");
-    const navbarMenu = document.querySelector(".navbar-menu");
+const menu = document.getElementById("menu");
+const navbarMenu = document.querySelector(".navbar-menu");
 
-    menuButton.addEventListener("click", function () {
-        navbarMenu.style.display = (navbarMenu.style.display === "flex") ? "none" : "flex";
-    });
-});
+menu.onclick = function () {
+    if (navbarMenu.style.display == "flex") {
+        navbarMenu.style.display = "none";
+    } else {
+        navbarMenu.style.display = "flex";
+    }
+};
